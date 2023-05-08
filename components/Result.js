b0cdd7e18f2339d29c654d109446557a74681631
iioayne25 <pavinee_sut@cmu.ac.th>
2023-05-08 17:01:03 +0700

    calculte different day,month,year

--- components/Result.js
+++ components/Result.js
@@ -1,68 +1,14 @@
 import React, { useEffect, useState } from 'react'
 
-const Result = () => {
-  const [data,setData] = useState(null)
-
-  useEffect(()=>{
-    fetch('/api/form')
-    .then((res)=>res.json())
-    .then((data)=>{
-      setData(data);
-    })
-  },[]);
- console.log("data: ",data)
+const Result = ({data}) => {
   return (
-    <div className='flex flex-col self-left px-5 -space-y-6   text-left w-full'>
-      
-      
-      <p className='resultItems'>{data.year != "" ?(
-        data.year
-      ):(<span className='noResult'>-</span>)} years</p>
-      <p className='resultItems'>{data.day != "" ?(
-        data.day
-      ):(<span className='noResult'>-</span>)} days</p>
-      <p className='resultItems'>{data.month != "" ?(
-        data.month
-      ):(<span className='noResult'>-</span>)} months</p>
-    {/* {data?(
-      data.year?(
-        <p className='resultItems'>{data.year} years</p>
-      ):(
-        <p className='resultItems'>- year</p>
-      ),
-      data.day?(
-        <p className='resultItems'>{data.day} day</p>
-      ):(
-        <p className='resultItems'>- day</p>
-      ),
-      data.month?(
-        <p className='resultItems'>{data.month} month </p>
-      ):(
-        
-        <p className='resultItems'>- month </p>
-      )
-    ):("")} */}
-
-      
-      
-      
-      
+    <div className='flex flex-col self-left px-3 -space-y-2 text-left w-full'>
+      <p className="resultItems">{data.year != "" ?<span className='resultNumber'>{data.year}</span>  : <span className="resultNumber">{data.year === 0 ?("0"):"- -"} </span>} years</p>
+      <p className="resultItems">{data.month != "" ?  <span className='resultNumber'>{data.month}</span>: <span className="resultNumber">- -</span>} months</p>
+      <p className="resultItems">{data.day != "" ? <span className='resultNumber'>{data.day}</span> : <span className="resultNumber">- -</span>} days</p>
       
     </div>
   )
 }
 
 export default Result
-// export async function getServerSideProps(context){
-//   const res = await fetch('http://localhost:3000/api/form')
-//   const data = await res.json();
-
-//   if(!data){
-//     return {
-//       notFound:true,
-//     }
-//   }
-//   return {
-//     props:{data},
-//   }
-// }
